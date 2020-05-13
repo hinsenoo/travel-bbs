7b611592efffb80b5f3403250cba6b6b262b7dd4
hinsenoo <837545254@qq.com>
2020-05-13 23:04:12 +0800

    反向代理，接口拦截

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import VueLazyLoad from 'vue-lazyload';
 import axios from 'axios';
 import VueAxios from 'vue-axios'
-import { Menu,MenuItem,Button,Input,Avatar,Dropdown,DropdownItem,DropdownMenu,Carousel,CarouselItem,Tabs,TabPane,Message,Upload } from 'element-ui';
+import { Menu,MenuItem,Button,Input,Avatar,Dropdown,DropdownItem,DropdownMenu,Carousel,CarouselItem,Tabs,TabPane,Message,Upload,Select,Option } from 'element-ui';
 // 确保引入样式
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router';
@@ -22,6 +22,8 @@ Vue.use(CarouselItem);
 Vue.use(Tabs);
 Vue.use(TabPane);
 Vue.use(Upload);
+Vue.use(Select);
+Vue.use(Option);
 Vue.component(Message.name, Message);
 // 挂载的 vue 实例的原型
 Vue.prototype.$message = Message;
@@ -32,6 +34,29 @@ Vue.use(VueAxios,axios);
 Vue.use(VueLazyLoad,{
   loading: '/loading/loading-bubbles.svg',
   error: 'https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png'
+});
+
+// 超时设置 8s
+axios.defaults.timeout = 8000;
+// 根据环境变量获取不同的请求地址
+// axios.defaults.baseURL = env.baseURL;
+// 接口错误拦截
+axios.interceptors.response.use(function (response){
+  // 直接取得响应的 data
+  let res = response.data;
+  // 业务错误拦截
+  if(response.status == 200){
+    return res;
+  }else{
+    // 抛出异常
+    Message.warning(res.msg);
+    return Promise.reject(res);
+  }
+},(error)=>{
+  // 请求错误拦截 http 状态码
+  let res = error.response;
+  Message.error(res.data.message);
+  return Promise.reject(error);
 });
 
 // 生产环境提示
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    devServer: {
+        host: 'localhost',
+        port: 8080,
+        // 反向代理
+        proxy: {
+            //拦截 /api 的访问
+            // 例 https://localhost:8080/api/loading
+            // 当访问到 /api 时会转发到 target
+            '/api': {
+                // 代理的目标地址
+                target: 'http://47.106.215.69:8080/',
+                // 是否要将主机头的源点更改为 url 地址
+                changeOrigin: true,
+                // 路径转发规则：把 /api 置为空
+                pathRewrite: {
+                    '/api': ''
+                },
+            }
+        }
+    }
+}
